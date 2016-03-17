0b140b4c41d1411855278ff43017ed4e23e46c08
Mark Karavan <mark.karavan@gmail.com>
2016-03-17 02:55:50 +0000

    adds signup

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var players = {};
+var players = {'a':{}};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   socket.on('initialize', function(msg) {
     console.log(msg);
-    io.emit('playerList', players);
+    updatePlayers;
+  });
+
+  socket.on('signup', function(username) {
+    if (Object.keys(players).indexOf(username) == -1) {
+      // player does not exist, create the player
+      socket.nickname = username; 
+      players[socket.nickname] = socket;
+      io.to(socket.id).emit('signup successful', socket.nickname);
+      updatePlayers();
+    } else {
+      // player exists, send error
+      io.to(socket.id).emit('signup error', '"' + username + '" is already taken.');
+    }
   });
 
   socket.on('x', function(x) {
@@ -19,6 +32,11 @@ io.on('connection', function(socket){
   socket.on('y', function(y) {
     console.log("y=" + y);
   });
+
+  function updatePlayers() {
+    io.emit('playerList', Object.keys(players));
+  }
+
 });
 
 http.listen(3000, function(){
